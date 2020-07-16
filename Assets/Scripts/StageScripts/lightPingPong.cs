a5f71b3dfc61b6f6744aa2676cc1d9cd724409fe
Lenny Smith <lenny.elias.s@gmail.com>
2020-07-16 14:33:03 +0300

    Audio

---
+++ Assets/Scripts/StageScripts/lightPingPong.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightPingPong : MonoBehaviour
+{
+
+    public Transform lightObj;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lightObj.position = new Vector2(Mathf.PingPong(Time.time * speed, 100), transform.position.y);
+    }
+}
